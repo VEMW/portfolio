938a9467df588ef1d23eea4285006df47b58d1b1
VEMW <34155399+VEMW@users.noreply.github.com>
2018-11-20 20:33:12 +0100

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,31 @@
+let modaal = document.querySelector('#modaalAchtergrond');
+let modaalVenster = document.querySelector('#modaalVenster');
+let modaalInhoud = document.querySelector('#modaalInhoud');
+let modaalTriggers = document.querySelectorAll('.trigger');
+let alleModalen = new Object();
+
+const sluitModaal = () => {
+  modaalInhoud.innerHTML = "";
+  modaal.parentNode.removeChild(modaal);
+}
+modaalVenster.addEventListener('click', (e) => {
+  e.stopPropagation();
+})
+sluitKnop.addEventListener('click', sluitModaal);
+modaal.addEventListener('click', sluitModaal);
+
+for(let i=0; i<modaalTriggers.length; i++) {
+  let item = modaalTriggers[i].hash.substr(1);
+  alleModalen[item] = document.getElementById(item);
+  modaalTriggers[i].addEventListener('click', (e)=> {
+    e.preventDefault();
+    modaalInhoud.appendChild(alleModalen[item]);
+    document.body.appendChild(modaal);
+  })
+}
+
+for(elem in alleModalen) {
+  alleModalen[elem].parentNode.removeChild(alleModalen[elem]);
+}
+
+sluitModaal();
